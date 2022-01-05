fd90de93e558e74a73009f0ddf3c2a3fbd13afac
Snakeheart404 <polina.glazunova@yahoo.com>
2022-01-05 19:05:37 +0200

    .

--- rollup.config.js
+++ rollup.config.js
@@ -42,36 +42,23 @@ export default {
     },
     plugins: [
         svelte({
-            compilerOptions: {
-                // enable run-time checks when not in production
-                dev: !production,
+            dev: !production,
+            // Change css bundle location
+            css: css => {
+                css.write('../public/build/bundle.css');
             },
         }),
-        // we'll extract any component CSS out into
-        // a separate file - better for performance
-        css({ output: 'bundle.css' }),
-
-        // If you have external dependencies installed from
-        // npm, you'll most likely need these plugins. In
-        // some cases you'll need additional configuration -
-        // consult the documentation for details:
-        // https://github.com/rollup/plugins/tree/master/packages/commonjs
         resolve({
             browser: true,
             dedupe: ['svelte'],
         }),
         commonjs(),
-
-        // In dev mode, call `npm run start` once
-        // the bundle has been generated
         !production && serve(),
 
-        // Watch the `public` directory and refresh the
-        // browser on changes when not in production
-        !production && livereload('public'),
+        // Watch the new `public` directory and refresh
+        // the browser on changes
+        !production && livereload('../public'),
 
-        // If we're building for production (npm run build
-        // instead of npm run dev), minify
         production && terser(),
     ],
     watch: {
