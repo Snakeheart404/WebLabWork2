f1a0ea5c67623b4170cd5fe74340f169eb059a3a
Snakeheart404 <polina.glazunova@yahoo.com>
2022-01-09 21:18:31 +0200

    it works?

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,81 @@
+<script>
+	import loader from '$lib/loader.gif';
+	let form = {
+		reset: () => {}
+	};
+	let textError = '';
+	let showSpinner = false;
+	let statusMessage = false;
+	let formBtnDisable = false;
+	function resetFormStatus() {
+		statusMessage = false;
+		formBtnDisable = false;
+	}
+	let contactFormHandler = async (e) => {
+		formBtnDisable = true;
+		showSpinner = true;
+		statusMessage = false;
+		const referrerVal = document.referrer;
+		let formData = { referrer: referrerVal };
+		Array.from(form.elements)
+			.filter((e) => e.tagName !== 'BUTTON')
+			.forEach((e) => {
+				formData[e.name] = e.value;
+			});
+		try {
+			await fetch('/api/sendmail', {
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(formData),
+				method: 'POST'
+			}).then((res) => {
+				if (res.ok) {
+					return res;
+				}
+				throw res;
+			});
+			statusMessage = true;
+			e.target.reset();
+		} catch (e) {
+			if (e.status >= 500) {
+				textError = 'Error Server';
+			} else if (e.status === 400) {
+				textError = 'Empty email message!';
+			} else if (e.status === 429) {
+				textError = 'Send too many mail';
+			}
+			statusMessage = false;
+			console.log(e);
+		} finally {
+			showSpinner = false;
+			formBtnDisable = false;
+		}
+	};
+</script>
+
 <main>
-	<form>
+	<form bind:this={form} on:submit|preventDefault={contactFormHandler}>
 		<h1>Fill in the form:</h1>
-		<input type="text" name="name" placeholder="Name" />
-		<input type="email" name="email" placeholder="Email" />
-		<textarea name="text" placeholder="Enter your mesage..." />
-		<button>Send</button>
+		<input type="text" name="name" placeholder="Name" required />
+		<input type="email" name="email" placeholder="Email" required />
+		<textarea name="text" placeholder="Enter your mesage..." required />
+		<button type="submit" disabled={formBtnDisable}>{#if showSpinner}
+			<img src={loader} alt="loader" />
+		{:else if true}
+			Send
+		{/if}</button>
+		{#if statusMessage}
+			<p class="status-text success">
+				Message sent!
+				<button on:click={resetFormStatus}> &times; </button>
+			</p>
+		{:else if textError != ""}
+			<p class="status-text error">
+				{textError}
+				<button on:click={resetFormStatus}> &times; </button>
+			</p>
+		{/if}
 	</form>
 </main>
 
